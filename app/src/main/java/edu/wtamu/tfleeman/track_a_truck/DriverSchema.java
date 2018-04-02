3e45a2b807b24dbf4d78cec048bf5f546009ff7f
Christian Loya <caloya1@buffs.wtamu.edu>
2018-04-02 14:44:12 -0500

    Updated Schema Models

--- app/src/main/java/edu/wtamu/tfleeman/track_a_truck/DriverSchema.java
+++ app/src/main/java/edu/wtamu/tfleeman/track_a_truck/DriverSchema.java
@@ -1,4 +1,18 @@
 package edu.wtamu.tfleeman.track_a_truck;
 
 public class DriverSchema {
+
+    public static final class DriverTable{
+        public static final String NAME = "Driver";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String Fname = "fname";
+            public static final String Lname = "lnamne";
+            public static final String Phone = "Phone";
+            public static final String Email = "Email";
+            public static final String Address = "Address";
+
+        }
+    }
 }
