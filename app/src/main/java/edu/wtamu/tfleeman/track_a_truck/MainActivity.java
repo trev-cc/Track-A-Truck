0698fbef17fd5da02051e67decc0175d21a20193
Christian Loya <caloya1@buffs.wtamu.edu>
2018-05-03 11:19:47 -0500

    "API and Button features-Christian"

--- app/src/main/java/edu/wtamu/tfleeman/track_a_truck/MainActivity.java
+++ app/src/main/java/edu/wtamu/tfleeman/track_a_truck/MainActivity.java
@@ -1,13 +1,31 @@
 package edu.wtamu.tfleeman.track_a_truck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MapsActivity2.class);
+                startActivity(i);
+            }
+
+        });
     }
-}
+
+    }
+
+
